da83eb515281e715d29c91be75678d390d631ca0
mckinneyjaiden5418 <spiderbeary@gmail.com>
2026-03-04 11:43:28 -0500

    Setup.

---
+++ DSALab4/search_algorithms.cpp
@@ -0,0 +1,12 @@
+#include "searching_algorithms.h"
+
+namespace iterative {
+	int binary_search(const int arr[], int size, int target) {
+
+	}
+};
+namespace recursive {
+	int binary_search(const int arr[], int size, int target) {
+
+	}
+};
